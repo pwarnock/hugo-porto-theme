d26caf452d97a1d79dbab3e6a4778b5ddb77088e
Pete Warnock <github@peterwarnock.com>
2025-10-13 12:24:05 -0700

    Include site layouts in Tailwind content for dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./layouts/**/*.html'],
+  content: ['./layouts/**/*.html', '../../layouts/**/*.html'],
   plugins: [],
   darkMode: 'class',
 };
