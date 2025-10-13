b0a7d340c6df58e18699569d8ba0228608da8f41
Pete Warnock <github@peterwarnock.com>
2025-10-13 16:57:16 -0700

    fix: add safelist for dark mode classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,13 @@
 module.exports = {
   content: ['./layouts/**/*.html', '../../layouts/**/*.html'],
   darkMode: 'class',
+  safelist: [
+    'dark:bg-gray-800',
+    'dark:text-gray-100',
+    'dark:text-gray-300',
+    'dark:border-gray-600',
+    'dark:bg-gray-700',
+    'dark:text-gray-200',
+  ],
   plugins: [],
 };
