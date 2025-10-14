d3c2249ad12f7c4418d7aeb73eb5a11f350d5e7b
Pete Warnock <github@peterwarnock.com>
2025-10-13 17:03:37 -0700

    fix: load tailwind.scss and clean config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,5 @@
 module.exports = {
   content: ['./layouts/**/*.html', '../../layouts/**/*.html'],
   darkMode: 'class',
-  safelist: [
-    'dark:bg-gray-800',
-    'dark:text-gray-100',
-    'dark:text-gray-300',
-    'dark:border-gray-600',
-    'dark:bg-gray-700',
-    'dark:text-gray-200',
-  ],
   plugins: [],
 };
